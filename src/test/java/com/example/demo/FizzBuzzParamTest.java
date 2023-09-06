5bd48dd4e45680d0924082eb4a2fca2ed0277693
supaket <supaket@enersys.co.th>
2023-09-06 12:19:31 +0700

    Add parameterized test

---
+++ src/test/java/com/example/demo/FizzBuzzParamTest.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class FizzBuzzParamTest {
+    FizzBuzz fizzBuzz;
+
+//    @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+//    @Retention(RetentionPolicy.RUNTIME)
+//    public @interface ArraySource {
+//        int[] array() default {};
+//    }
+      @BeforeEach
+    private void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            "5, Buzz",
+            "7, 7",
+            "9, Fizz",
+            "15, FizzBuzz"
+    })
+    public void ShouldSayFizzBuzAsAnInputProvided(int pInput, String pExpected){
+        String result = fizzBuzz.say(pInput);
+        assertEquals(pExpected, result);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources="/input.csv")
+    public void ShouldSayFizzBuzWithCsvFilesInput(int pInput, String pExpected){
+        String result = fizzBuzz.say(pInput);
+        assertEquals(pExpected, result);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfInputIs0(){
+        int input = 0;
+
+        IllegalStateException thrown = Assertions.assertThrows(IllegalStateException.class, () -> {
+            //Code under test
+            fizzBuzz.say(input);
+        });
+
+        assertEquals("Zero is not allowed",thrown.getMessage());
+    }
+}
